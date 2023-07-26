a4da0e70b7c8f1dba607eec88b0e412488787653
Muhammad Saqlain <107932964+saqlain017@users.noreply.github.com>
2023-07-26 20:04:32 +0500

    Add files via upload

---
+++ src/Components/Forms.jsx
@@ -0,0 +1,38 @@
+import { useForm } from "react-hook-form"
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers/yup'
+const Forms = ()=>{
+    const schema = yup.object().shape(
+        {
+            fullName: yup.string().required(),
+            email: yup.string().email().required(),
+            age : yup.number().positive().integer().min(18).required(),
+            password: yup.string().min(8).max(20).required(),
+            confirmPassword: yup.string().oneOf([yup.ref("password"),null],"Password Don't Match").required(),
+        }
+    )
+    const {register, handleSubmit, formState:{errors}} = useForm({
+        resolver: yupResolver(schema),
+    });
+    const onSubmit = (data)=>{
+        console.log(data)
+    }
+    return(
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Name" {...register("fullName")}/>
+                <p>{errors.fullName?.message}</p>
+                <input type="email" placeholder="Email"{...register("email")}/>
+                <p>{errors.email?.message}</p>
+                <input type="number" placeholder="Age"{...register("age")}/>
+                <p>{errors.age?.message}</p>
+                <input type="password" placeholder="Password"{...register("password")}/>
+                <p>{errors.password?.message}</p>
+                <input type="password" placeholder="Re-type Password" {...register("confirmPassword")}/>
+                <p>{errors.confirmPassword?.message}</p>
+                <input type="submit"/>
+            </form>
+        </>
+    )
+}
+export default Forms
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.css'
+import Forms from './Components/Forms'
+
+export const App = ()=>{
+
+    return(
+        <>
+        <Forms />
+        </>
+    )
+}
